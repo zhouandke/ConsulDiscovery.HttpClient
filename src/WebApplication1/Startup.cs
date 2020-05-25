fd0a31056b0347549500046d890728a57cbd5b93
zhouandke <zhouandke_163@163.com>
2020-05-25 16:20:36 +0800

    Clear code

--- src/WebApplication1/Startup.cs
+++ src/WebApplication1/Startup.cs
@@ -17,7 +17,6 @@ namespace WebApplication1
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -32,7 +31,6 @@ namespace WebApplication1
                 .AddHttpMessageHandler<DiscoveryHttpMessageHandler>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
         {
             if (env.IsDevelopment())
